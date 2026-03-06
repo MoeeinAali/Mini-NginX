43be9562221942176df94f19c006be040eb95142
Moeein <moeeeinaali@gmail.com>
2026-03-06 13:31:36 +0330

    feat: add implementation of reverseproxy handler

--- internal/handlers/reverseproxy.go
+++ internal/handlers/reverseproxy.go
@@ -2,7 +2,29 @@ package handlers
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"net"
 )
 
-func ReverseProxy(client net.Conn, reader *bufio.Reader, target string, firstLine string) {}
+func ReverseProxy(client net.Conn, reader *bufio.Reader, target string, firstLine string) {
+	backend, err := net.Dial("tcp", target)
+	if err != nil {
+		resp := "HTTP/1.1 502 Bad Gateway\r\nContent-Length: 11\r\n\r\nBad Gateway"
+		client.Write([]byte(resp))
+		return
+	}
+	defer backend.Close()
+	fmt.Fprint(backend, firstLine)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Fprint(backend, line)
+		if line == "\r\n" {
+			break
+		}
+	}
+	io.Copy(client, backend)
+}
