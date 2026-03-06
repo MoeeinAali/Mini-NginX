e23a72a6cdd9f392260a4c545dcef15c140093a9
Moeein <moeeeinaali@gmail.com>
2026-03-06 12:50:57 +0330

    feat: add config.go adn yaml parser

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ListenOn int              `yaml:"listen_on"`
+	Paths    map[string]Route `yaml:"paths"`
+}
+
+type Route struct {
+	Type   string `yaml:"type"`
+	Target string `yaml:"target"`
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
