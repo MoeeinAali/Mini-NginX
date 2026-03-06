5f8bca2cbc4f48af8e1e49a10d0b4c5f37b55cc8
Moeein <moeeeinaali@gmail.com>
2026-03-06 13:34:18 +0330

    feat: add main cmd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"Mini-NginX/internal/config"
+	"Mini-NginX/internal/logger"
+	"Mini-NginX/internal/server"
+	"log"
+)
+
 func main() {
+	cfg, err := config.Load("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger.Init("server.log")
+
+	s := server.New(cfg)
+	logger.Info("Server Starting", "port", cfg.ListenOn)
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
