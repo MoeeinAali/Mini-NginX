1721fba18615a885bf0d440623336f3146110449
Moeein <moeeeinaali@gmail.com>
2026-03-06 13:13:18 +0330

    feat: add router package

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,7 +2,51 @@ package router
 
 import (
 	"Mini-NginX/internal/config"
+	"Mini-NginX/internal/handlers"
+	"Mini-NginX/internal/logger"
+	"bufio"
+	"fmt"
 	"net"
+	"strings"
 )
 
-func Handle(conn net.Conn, cfg *config.Config) {}
+func Handle(conn net.Conn, cfg *config.Config) {
+	reader := bufio.NewReader(conn)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	parts := strings.Split(line, " ")
+	if len(parts) != 2 {
+		return
+	}
+	method := parts[0]
+	path := parts[1]
+	logger.Info("request method: %s, path: %s", method, path)
+
+	for route, rule := range cfg.Paths {
+		if strings.HasPrefix(path, route) {
+			logger.Info("route matched", "route", route, "type", rule.Type)
+
+			switch rule.Type {
+			case "redirect":
+				handlers.Redirect(conn, rule.Target)
+
+			case "staticfile":
+				handlers.StaticFile(conn, rule.Target)
+			case "servedir":
+				handlers.ServeDir(conn, rule.Target)
+			case "reverseproxy":
+				handlers.ReverseProxy(conn, reader, rule.Target, line)
+			default:
+				write404(conn)
+			}
+		}
+	}
+}
+
+func write404(conn net.Conn) {
+	body := "404 not found"
+	resp := fmt.Sprintf("HTTP/1.1 404 Not Found\r\nContent-Length: %d\r\n\r\n", len(body), body)
+	conn.Write([]byte(resp))
+}
