2978844f1b495466916ca88156b5aa29dec5f98f
Moeein <moeeeinaali@gmail.com>
2026-03-06 13:01:09 +0330

    feat: add router and server internal

---
+++ internal/router/router.go
@@ -0,0 +1,8 @@
+package router
+
+import (
+	"Mini-NginX/internal/config"
+	"net"
+)
+
+func Handle(conn net.Conn, cfg *config.Config) {}
---
+++ internal/server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"Mini-NginX/internal/config"
+	"Mini-NginX/internal/logger"
+	"Mini-NginX/internal/router"
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	cfg *config.Config
+}
+
+func New(cfg *config.Config) *Server {
+	return &Server{cfg: cfg}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", s.cfg.ListenOn))
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		logger.Info("Connection accepted", "remote", conn.RemoteAddr().String())
+		go handle(conn, s.cfg)
+	}
+}
+
+func handle(conn net.Conn, cfg *config.Config) {
+	defer conn.Close()
+	router.Handle(conn, cfg)
+}
